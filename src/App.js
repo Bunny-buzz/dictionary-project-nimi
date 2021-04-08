6c1d1aa02edac826946c5fb970370f1452f0540d
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2021-04-07 23:37:16 -0600

    added folder Dictionary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
         <footer className="text-center">
           Coded by Nimi Dada, open-sourced
         </footer>
@@ -12,5 +16,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
